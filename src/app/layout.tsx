a164a7f0ead0a172cd29d8cb01f8116845083358
aghmnl <aghmnl@gmail.com>
2025-04-20 21:08:38 -0300

    trpcClient set but still not working

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,18 @@
 import type { Metadata } from "next";
 import { Red_Hat_Display } from "next/font/google";
 import "./globals.css";
+import { trpc } from "../utils/trpc";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from "react";
+import { httpBatchLink } from "@trpc/client";
 
 const redHatDisplay = Red_Hat_Display({
   variable: "--font-red-hat-display",
   subsets: ["latin"],
 });
+
+const queryClient = new QueryClient();
+const trpcClient = trpc.createClient({ links: [httpBatchLink({ url: "http://localhost:4000/trpc" })] });
 
 export const metadata: Metadata = {
   title: "Task Management App",
@@ -15,7 +22,13 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
-      <body className={`${redHatDisplay.variable}  antialiased`}>{children}</body>
+      <body className={`${redHatDisplay.variable}  antialiased`}>
+        <QueryClientProvider client={queryClient}>
+          <trpc.Provider client={trpcClient} queryClient={queryClient}>
+            {children}
+          </trpc.Provider>
+        </QueryClientProvider>
+      </body>
     </html>
   );
 }
